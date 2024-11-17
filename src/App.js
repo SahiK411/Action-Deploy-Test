51984969a881876e2d099c41689f2e16a549f43c
Gabriel Martinez <gabriel.emv411@gmail.com>
2024-11-17 12:50:45 -0600

    Add: Page functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [shortFlag, setShortFlag] = useState(false);
+  const [longFlag, setLongFlag] = useState(false);
+
+  function onNameChange(e){
+    setName(e.target.value);
+  }
+
+  function NameResponse({sF, lF}){
+    if(sF){
+      return (<p id='pageShort' data-testid="pageShort">What a short name!</p>);
+    }
+    if(lF){
+      return (<p id='pageLong'data-testid="pageLong">What a long name!</p>);
+    }
+    return null;
+  }
+
+  function validateName(){
+    if(name.length < 5){
+      setShortFlag(true)
+      setLongFlag(false)
+    }
+    else{
+      setShortFlag(false)
+      setLongFlag(true)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome!
         </p>
         <p>
-          Test
+          Please enter your name
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div id='pageBody' className='App-body'>
+        <label for="textInput">Your Name</label>
+        <input type="text" id='textInput' onChange={onNameChange} data-testid="textInput"/>
+        <input type='button' onClick={validateName} value="Submit" className='input-button' data-testid="inputButton"/>
+        <NameResponse sF={shortFlag} lF={longFlag}/>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react logo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoElement = screen.getByAltText("logo");
+  expect(logoElement).toBeInTheDocument();
+});
+
+test('renders short name paragraph', () => {  
+  render(<App />);
+  const inputElement = screen.getByTestId("textInput")
+  const inputButton = screen.getByTestId("inputButton")
+  fireEvent.change(inputElement, {target:{value:"AA"}})
+  fireEvent.click(inputButton)
+  const shortElement = screen.getByTestId("pageShort")
+  expect(shortElement).toBeInTheDocument();
+});
+
+test('renders long  name paragraph', () => {  
+  render(<App />);
+  const inputElement = screen.getByTestId("textInput")
+  const inputButton = screen.getByTestId("inputButton")
+  fireEvent.change(inputElement, {target:{value:"AAAAAA"}})
+  fireEvent.click(inputButton)
+  const longElement = screen.getByTestId("pageLong")
+  expect(longElement).toBeInTheDocument();
 });
